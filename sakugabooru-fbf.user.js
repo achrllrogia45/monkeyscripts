dbd41c2adbcdd210f39c24d2ee6b27f153f3e640
Hilman Nur Firdaus <116096876+achrllrogia45@users.noreply.github.com>
2026-06-06 03:34:18 +0700

    Update author name and formatting in user script

--- sakugabooru-fbf.user.js
+++ sakugabooru-fbf.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Adds , and . hotkeys to Sakugabooru frame buttons
-// @author       You
+// @author       achrllrogia45
 // @match        https://*.sakugabooru.com/post/show/*
 // @grant        none
 // @updateURL    https://github.com/achrllrogia45/monkeyscripts/raw/main/sakugabooru-fbf.user.js
